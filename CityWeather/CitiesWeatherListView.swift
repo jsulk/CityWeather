ed170a6eabebf2864fce1355e8510f1862966b81
Jake Sulkoske <jakesulkoske@Jakes-MBP.attlocal.net>
2022-04-02 21:10:19 -0700

    removing file

--- CityWeather/CitiesWeatherListView.swift
+++ CityWeather/CitiesWeatherListView.swift
@@ -5,41 +5,4 @@
 //  Created by Jake Sulkoske on 4/2/22.
 //
 
-import SwiftUI
-
-struct CitiesWeatherListView: View {
-    
-    
-    var body: some View {
-        NavigationView {
-            VStack {
-                citiesListView
-            }
-            .navigationTitle("Forecasts")
-            .toolbar {
-                self.navigationButton
-            }
-        }
-    }
-    
-    @ViewBuilder
-    private var citiesListView: some View {
-        List {
-            Text("City 1")
-            Text("City 2")
-        }
-    }
-    
-    @ViewBuilder
-    var navigationButton: some View {
-        NavigationLink(destination: CitiesListView()) {
-            Text("Cities")
-        }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        CitiesWeatherListView()
-    }
-}
+import Foundation
