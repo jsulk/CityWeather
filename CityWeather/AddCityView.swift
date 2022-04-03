cbf409076fcca31d93820aca88f15dfbcd88a982
Jake Sulkoske <jakesulkoske@Jakes-MBP.attlocal.net>
2022-04-02 19:56:17 -0700

    Added first functional screens

---
+++ CityWeather/AddCityView.swift
@@ -0,0 +1,20 @@
+//
+//  AddCityView.swift
+//  CityWeather
+//
+//  Created by Jake Sulkoske on 4/2/22.
+//
+
+import SwiftUI
+
+struct AddCityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddCityView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddCityView()
+    }
+}
