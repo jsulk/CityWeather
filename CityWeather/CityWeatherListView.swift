a41ccfbc4af4fbba3f35f51f2b463dac6fdc4d94
Jake Sulkoske <jakesulkoske@Jakes-MBP.attlocal.net>
2022-04-02 21:08:58 -0700

    Updating lost files

---
+++ CityWeather/CityWeatherListView.swift
@@ -0,0 +1,46 @@
+//
+//  CityWeatherListView.swift
+//  CityWeather
+//
+//  Created by Jake Sulkoske on 4/2/22.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct CitiesWeatherListView: View {
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                citiesListView
+            }
+            .navigationTitle("Forecasts")
+            .toolbar {
+                self.navigationButton
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private var citiesListView: some View {
+        List {
+            Text("City 1")
+            Text("City 2")
+        }
+    }
+    
+    @ViewBuilder
+    var navigationButton: some View {
+        NavigationLink(destination: CitiesListView()) {
+            Text("Cities")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CitiesWeatherListView()
+    }
+}
